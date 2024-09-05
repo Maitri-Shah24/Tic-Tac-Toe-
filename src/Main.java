d5c72abd52b92fc9a292bad127d6c7b4ad15bf63
= <=>
2024-09-05 11:49:48 +0530

    placepiece method to put your symbol + random function for cpu created

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -14,40 +15,14 @@ public class Main {
         int pos =  s.nextInt();
         System.out.println(pos);
 
-        switch (pos)
-        {
-            case 1:
-                board[0][0] = 'X';
-                break;
-            case 2:
-                board[0][2] = 'X';
-                break;
-            case 3:
-                board[0][4] = 'X';
-                break;
-            case 4:
-                board[2][0] = 'X';
-                break;
-            case 5:
-                board[2][2] = 'X';
-                break;
-            case 6:
-                board[2][4] = 'X';
-                break;
-            case 7:
-                board[4][0] = 'X';
-                break;
-            case 8:
-                board[4][2] = 'X';
-                break;
-            case 9:
-                board[4][4] = 'X';
-                break;
-        }
-        printGameboard(board);
+        placePiece(board, pos, "player");
+        Random rand = new Random();
+        int cpuPos = rand.nextInt(9)+ 1;
+        placePiece(board, cpuPos, "cpu");
+        printBoard(board);
 
     }
-    public static void printGameboard(char[][] board)
+    public static void printBoard(char[][] board)
     {
         for(char[] row :board)
         {
@@ -56,6 +31,50 @@ public class Main {
                 System.out.print(c);
             }
             System.out.println();
+        }
+    }
+    public static void placePiece(char[][] board,int pos, String user)
+    {
+        char symbol = ' ';
+        if(user.equals("player"))
+        {
+            symbol = 'X';
+        }
+        else if(user.equals("cpu"))
+        {
+            symbol = 'O';
+        }
+        switch (pos)
+        {
+            case 1:
+                board[0][0] = symbol;
+                break;
+            case 2:
+                board[0][2] = symbol;
+                break;
+            case 3:
+                board[0][4] = symbol;
+                break;
+            case 4:
+                board[2][0] = symbol;
+                break;
+            case 5:
+                board[2][2] = symbol;
+                break;
+            case 6:
+                board[2][4] = symbol;
+                break;
+            case 7:
+                board[4][0] = symbol;
+                break;
+            case 8:
+                board[4][2] = symbol;
+                break;
+            case 9:
+                board[4][4] = symbol;
+                break;
+            default:
+                break;
         }
     }
 }
