33545b158f1b1eb8abba6c587cdb93f362ee641c
= <=>
2024-09-06 10:25:13 +0530

    Problem in Player file

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Player {
+import java.util.ArrayList;
+import java.util.List;
+
+class Player {
+    private String name;
+    private final List<Integer> positions;
+
+    public Player() {
+        positions = new ArrayList<>();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addPosition(int pos) {
+        positions.add(pos);
+    }
+
+    public List<Integer> getPositions() {
+        return positions;
+    }
+
+    public void resetPositions() {
+        positions.clear();
+    }
 }
+
